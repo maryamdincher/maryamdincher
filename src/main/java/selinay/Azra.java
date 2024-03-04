d21a36f3fd9daa5618633267fe1ed8f336317efd
maryamdincher <maryamdincher@gmail.com>
2024-03-04 15:56:11 -0700

    h

--- src/main/java/selinay/Azra.java
+++ src/main/java/selinay/Azra.java
@@ -2,8 +2,10 @@ package selinay;
 
 public class Azra {
     public static void main(String[] args) {
-      //  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
+     //   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
         System.out.println("Hello World");
         System.out.println("Welcome");
+
+
     }
 }
